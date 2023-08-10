7aed8f355a17a42a804407c41fb25344f49f278c
linmsen <819747263@qq.com>
2023-08-10 17:08:57 +0800

    优化

--- mik-fr/mik-fr-core/src/main/java/com/mik/core/pojo/PageInput.java
+++ mik-fr/mik-fr-core/src/main/java/com/mik/core/pojo/PageInput.java
@@ -6,4 +6,16 @@ import lombok.Data;
 public class PageInput {
     private Integer pageNum;
     private Integer pageSize;
+
+    public PageInput() {
+    }
+
+    public PageInput(Integer pageNum, Integer pageSize) {
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+    }
+
+    public static PageInput of(Integer pageNum, Integer pageSize){
+        return new PageInput(pageNum, pageSize);
+    }
 }
